59e12ec433dd3c15e4d51bbf5808046c1827157b
Your Name <you@example.com>
2020-12-03 17:33:56 -0500

    day 3 part 1 solution

--- day_03_pt2.py
+++ day_03_pt2.py
@@ -2,9 +2,14 @@ with open("input_03.txt", "r") as f:
     list = f.read().splitlines()
     count = 0
     index = 0
-    for line in list:
+    iterator = iter(list)
+    for line in iterator:
         line = line.strip()
         if line[index] is "#":
             count += 1
-        index = (index + 3) % len(line)
+        index = (index + 1) % len(line)
+        try:
+            next(iterator)
+        except StopIteration:
+            continue
     print("There are: " + str(count) + " trees in your path")
